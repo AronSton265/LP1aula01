be6484a04c5f7c5cc8ce449db64bee9c005d58d1
AronSton265 <AronSton265@users.noreply.github.com>
2020-02-27 13:42:10 +0000

    strings simples e verbatims

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			string a = "A";
+			string b = "B";
+			string c = "Hello";
+			string d = @"C:\Users\pedro\Downloads";
+			string e = "\u0061 b \u0063 d";
+			Console.WriteLine(a);
+			Console.WriteLine(b);
+			Console.WriteLine(c);
+			Console.WriteLine(d);
+			Console.WriteLine(e);
+		}
+    }
+}
