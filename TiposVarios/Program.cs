e23fe3d855b8c6659db154069e663415f9bac7ed
AronSton265 <AronSton265@users.noreply.github.com>
2020-02-27 13:36:28 +0000

    variaveis inteiras

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 4;
+            short c = 2;
+            uint d = 5;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+        }
+    }
+}
