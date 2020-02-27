e6e3a5ba005a63802161b181e9fe1ac9b3e527d1
AronSton265 <AronSton265@users.noreply.github.com>
2020-02-27 13:33:17 +0000

    /// = sumary

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -2,8 +2,15 @@
 
 namespace OlaMundo
 {
+    /// <summary>
+    /// chama a funcao main
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// apenas escreve hello world e bye wolrd
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
